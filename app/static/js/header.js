b9b34c67b427b7b9d51297a3c1e43d7ac3542e9e
babushkian <babushkian@mail.ru>
2024-04-09 18:37:19 +0600

    делаю ограничения для таймера

--- app/static/js/header.js
+++ app/static/js/header.js
@@ -9,10 +9,43 @@ const active = document.querySelector(".option_" + tableUrlEL.dataset.class);
 active.classList.add("option_active");
 
 const mainDate = document.querySelector("#main-date");
+const startDate = mainDate.getAttribute("min");
+const endDate = mainDate.getAttribute("max");
+
+const leftButtons = document.querySelectorAll("button.left");
+const rightButtons = document.querySelectorAll("button.right");
 const dayFofward = document.querySelector("#day-plus");
 const monthFofward = document.querySelector("#month-plus");
 const dayBackward = document.querySelector("#day-minus");
 const monthBackward = document.querySelector("#month-minus");
+const startDateButton = document.querySelector("#start_date");
+const endDateButton = document.querySelector("#end_date");
+
+function checkDisableButtons(leftDate, rightDate, buttonsSection) {
+  // даты адекватно не сравниваются, надо извращаться
+  const a = new Date(leftDate);
+  const b = new Date(rightDate);
+  if (a.getTime() >= b.getTime()) {
+    buttonsSection.forEach((button) => button.setAttribute("disabled", ""));
+  } else {
+    buttonsSection.forEach((button) => {
+      if (button.hasAttribute("disabled")) {
+        button.removeAttribute("disabled");
+      }
+    });
+  }
+}
+
+function checkDateInput() {
+  if (mainDate.value > endDate) {
+    mainDate.value = endDate;
+  }
+  if (mainDate.value < startDate) {
+    mainDate.value = startDate;
+  }
+  checkDisableButtons(startDate, mainDate.value, leftButtons);
+  checkDisableButtons(mainDate.value, endDate, rightButtons);
+}
 
 mainDate.addEventListener("keyup", function (e) {
   if (e.key === "Enter") {
@@ -28,7 +61,7 @@ dayFofward.addEventListener("click", () => {
   let date = new Date(mainDate.value);
   date.setDate(date.getDate() + 1);
   mainDate.valueAsDate = date;
-  getStat(tableUrl, mainDate.value);
+  getStat(tableUrl, this.value);
 });
 
 monthFofward.addEventListener("click", () => {
@@ -36,14 +69,14 @@ monthFofward.addEventListener("click", () => {
   const a = new Date(date.getFullYear(), date.getMonth() + 1, 0);
   date.setDate(date.getDate() + a.getDate());
   mainDate.valueAsDate = date;
-  getStat(tableUrl, mainDate.value);
+  getStat(tableUrl, this.value);
 });
 
 dayBackward.addEventListener("click", () => {
   let date = new Date(mainDate.value);
   date.setDate(date.getDate() - 1);
   mainDate.valueAsDate = date;
-  getStat(tableUrl, mainDate.value);
+  getStat(tableUrl, this.value);
 });
 
 monthBackward.addEventListener("click", () => {
@@ -51,12 +84,22 @@ monthBackward.addEventListener("click", () => {
   const a = new Date(date.getFullYear(), date.getMonth(), 0);
   date.setDate(date.getDate() - a.getDate());
   mainDate.valueAsDate = date;
-  getStat(tableUrl, mainDate.value);
+  getStat(tableUrl, this.value);
+});
+
+startDateButton.addEventListener("click", () => {
+  mainDate.value = startDate;
+  getStat(tableUrl, this.value);
+});
+endDateButton.addEventListener("click", () => {
+  mainDate.value = endDate;
+  getStat(tableUrl, this.value);
 });
 
 getStat(tableUrl, mainDate.value);
 
 async function getStat(url_string, date) {
+  checkDateInput();
   const URL = `http://127.0.0.1:5000/${url_string}/${date}`;
   console.log("API:", URL);
   const data = await fetch(URL);
